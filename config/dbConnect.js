69c571122716b587bb5e1225869ba8a2b386f6f4
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-11-13 12:56:36 +0200

    show stracture

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
-    const connected = mongoose.connect(process.env.MONGO_URL
-
-    );
+    const connected = mongoose.connect(process.env.MONGO_URL);
     console.log(`mongodb connected ${(await connected).connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
-    process.exit(1)
+    process.exit(1);
   }
 };
 
