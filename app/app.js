1b6a05b0aa0c5134898e3033553f13685543b3f4
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-11-13 02:22:48 +0200

    connect to mongodb e-commerce-nodejs

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
-import  express from "express"
-const app = express()
-export default app
+import express from "express";
+import dbConnect from "../config/dbConnect.js";
+import dotenv from "dotenv";
+// setup local env
+dotenv.config();
+//db connect
+dbConnect();
+const app = express();
+
+export default app;
