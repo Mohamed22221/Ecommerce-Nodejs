335b4baf6fb2bda13d48c4ce35d4efdee267b39a
Mohamed22221 <hamdymohame7@gmail.com>
2023-11-15 03:45:10 +0200

    handel glopal and routes error

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dbConnect from "../config/dbConnect.js";
 import dotenv from "dotenv";
 import userRoutes from "../routes/usersRoutes.js";
+import { ERROR } from "../utils/httpStatus.js";
 // setup local env
 dotenv.config();
 //db connect
@@ -12,4 +13,19 @@ app.use(express.json());
 //express routes
 app.use("/", userRoutes);
 
+// global middleware for handel error
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    status: error.status || ERROR,
+    message: error.message,
+    data: null,
+  });
+});
+// global middleware for not found routes
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: ERROR,
+    message: "Not Found Resource",
+  });
+});
 export default app;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,20 +1,20 @@
+import asyncWrapper from "../middlewares/asyncWrapper.js";
 import User from "../model/User.js";
 import bcrypt from "bcrypt";
+import sendError from "../utils/classError.js";
+import { ERROR, FAIL, SUCCESS } from "../utils/httpStatus.js";
 
 //@desc Controll regester
 //@route Post api/vi/users/regester
 //@access Private/admin
 
-export const regesterUser = async (req, res, next) => {
+export const regesterUser = asyncWrapper(async (req, res, next) => {
   const { fullName, email, password } = req.body;
   //check exist user
   const exist = await User.findOne({ email });
   if (exist) {
-    return res.status(400).json({
-      status: "error",
-      message: "User already exist",
-      data: null,
-    });
+    const error = sendError.create(400, ERROR, "User already exist");
+    return next(error);
   }
 
   //hash password using bcrypt
@@ -23,39 +23,33 @@ export const regesterUser = async (req, res, next) => {
   //create new user
   const user = await User.create({ fullName, email, password: hashedPassword });
   res.status(201).json({
-    status: "success",
+    status: SUCCESS,
     message: "User registerd successfully",
     data: user,
   });
-};
+});
 
 //@desc Controll login
 //@route Post api/vi/users/login
 //@access Public
 
-export const loginUser = async (req, res, next) => {
+export const loginUser = asyncWrapper(async (req, res, next) => {
   const { email, password } = req.body;
   //check found user
-  const userFound =await User.findOne({ email });
+  const userFound = await User.findOne({ email });
   if (!userFound) {
-    return res.status(400).json({
-      status: "error",
-      message: "Not found user",
-      data: null,
-    });
+    const error = sendError.create(400, ERROR, "Not found user");
+    return next(error);
   }
   const matchedPassword = await bcrypt.compare(password, userFound?.password);
-  if (userFound && matchedPassword ===true) {
+  if (userFound && matchedPassword === true) {
     return res.status(200).json({
       status: "success",
       message: "User logged in successfully",
       data: userFound,
     });
   } else {
-    return res.status(400).json({
-      status: "error",
-      message: "Invalid login",
-      data: null,
-    });
+    const error = sendError.create(400, ERROR, "Invalid login");
+    return next(error);
   }
-};
+});
