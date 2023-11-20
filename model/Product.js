a5122529285c9eccd249fadb4f4ed87c7a8f2ee0
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-11-21 01:07:16 +0200

    get avrage and total reviews

--- model/Product.js
+++ model/Product.js
@@ -65,7 +65,21 @@ const productSchema = new Schema(
     toJSON : {virtuals : true}
   }
 );
-
+//total rating
+productSchema.virtual("totalReviews").get(function() {
+  const product = this
+  return product?.reviews?.length
+})
+//average rating 
+productSchema.virtual("averageReviews").get(function() {
+  let totalRating = 0
+  const product = this
+  product?.reviews?.forEach((review) => {
+    totalRating += review?.rating
+  })
+  
+  return Number(totalRating / product.reviews.length) || 0
+})
 //compile the schema to model
 const Product = mongoose.model("Product", productSchema);
 export default Product;
